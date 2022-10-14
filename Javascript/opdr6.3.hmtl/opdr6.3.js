3767a4b9779085625e92591e73ceef79cdeeff6e
erikvdwiel <9019165@student.zadkine.nl>
2022-10-14 09:04:44 +0200

    6.3js

---
+++ Javascript/opdr6.3.hmtl/opdr6.3.js
@@ -0,0 +1,9 @@
+function changecolor(){
+    alert("de kleur wordt aangepast.");
+    console.log("het werkt ook in de console!");
+    Document.getElementById("tekst1").style.color = "red";
+}
+
+function Calculator(num,num2){
+    let me answer
+}
